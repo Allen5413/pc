9c23426ffea74e543a868ef165c2111cd6b32dd1
Allen <2319772333@qq.com>
2017-02-14 11:34:52 +0800

    登录验证

--- pc/src/main/java/com/allen/web/interceptor/LoginInterceptor.java
+++ pc/src/main/java/com/allen/web/interceptor/LoginInterceptor.java
@@ -1,5 +1,6 @@
 package com.allen.web.interceptor;
 
+import com.allen.util.StringUtil;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,12 +11,35 @@ import javax.servlet.http.HttpServletResponse;
  * 自定义拦截器1
  * Created by Allen on 2016/12/9.
  */
-public class MyInterceptor1 implements HandlerInterceptor {
+public class LoginInterceptor implements HandlerInterceptor {
+
+    //不需要拦截的路径
+    private static final String[] IGNORE_URI = {"/login", "/css"};
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println(">>>MyInterceptor1>>>>>>>在请求处理之前进行调用（Controller方法调用之前）");
         // 只有返回true才会继续向下执行，返回false取消当前请求
-        return true;
+        boolean flag = false;
+        String url = request.getRequestURL().toString();
+        for (String s : IGNORE_URI) {
+            if (url.contains(s)) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            if(url.lastIndexOf("/") == url.length()-1){
+                flag = true;
+            }else {
+                String loginName = null == request.getSession().getAttribute("loginName") ? "" : request.getSession().getAttribute("loginName").toString();
+                if (!StringUtil.isEmpty(loginName)) {
+                    flag = true;
+                } else {
+                    response.sendRedirect("/");
+                }
+            }
+        }
+        return flag;
     }
 
     @Override
