c48670eb7c70ab914a2834d94efcbc1f23ceb572
Allen5414 <525652880@qq.com>
2017-03-15 22:41:36 +0800

    *

--- pc/src/main/java/com/allen/entity/basic/FactoryDate.java
+++ pc/src/main/java/com/allen/entity/basic/FactoryDate.java
@@ -11,7 +11,7 @@ import java.util.Date;
  * Created by Allen on 2017/2/15 0015.
  */
 @Entity
-@Table(name = "t_eng_workcalfulldata")
+@Table(name = "factory_date")
 public class FactoryDate{
 
     /**
@@ -24,9 +24,85 @@ public class FactoryDate{
 
     @Id
     @GeneratedValue
-    private long FID;
+    private long id;
     private String year;
     private String month;
     private String day;
     private int isWork;
+    private String creator;
+    private Date createTime = new Date();
+    private String operator;
+    private Date operateTime = new Date();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public int getIsWork() {
+        return isWork;
+    }
+
+    public void setIsWork(int isWork) {
+        this.isWork = isWork;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public Date getOperateTime() {
+        return operateTime;
+    }
+
+    public void setOperateTime(Date operateTime) {
+        this.operateTime = operateTime;
+    }
 }
