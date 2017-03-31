f8200f8dd93909b4eee15a7f38a369293784c1ef
camelly <466696880@qq.com>
2017-03-31 10:19:22 +0800

    添加按钮权限设置功能

--- pc/src/main/java/com/allen/dao/basic/planorder/FindPlanOrderDao.java
+++ pc/src/main/java/com/allen/dao/basic/planorder/FindPlanOrderDao.java
@@ -31,10 +31,12 @@ public class FindPlanOrderDao extends BaseQueryDao {
                 "from T_BD_MATERIALBASE f,T_BD_MATERIAL a,T_BD_MATERIAL_L b,T_PLN_PLANORDER po left join T_PLN_PLANORDER_B pob on po.FID=pob.FID " +
                 "left join T_SAL_ORDER so on pob.FSALEORDERID=so.FID left join T_BD_CUSTOMER c on so.FCUSTID=c.FCUSTID " +
                 "where f.FMATERIALID = a.FMATERIALID AND a.FMATERIALID = b.FMATERIALID and b.FMATERIALID = po.FMATERIALID " +
-                "and po.FDOCUMENTSTATUS='A' and po.FRELEASETYPE=1 ORDER BY f.FSNO,po.FMATERIALID,po.FDEMANDDATE";
+                "and po.FDOCUMENTSTATUS='A' and po.FRELEASETYPE=1 AND po.FDEMANDDATE>=? and po.FDEMANDDATE<=?  ORDER BY f.FSNO,po.FMATERIALID,po.FDEMANDDATE";
 
         Session session = super.entityManager.unwrap(Session.class);
         SQLQuery sqlQuery = session.createSQLQuery(sql);
+        sqlQuery.setParameter(0,start);
+        sqlQuery.setParameter(1,end);
         List<Map> results = sqlQuery.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
         List<PlanOrder> planOrders = new ArrayList<PlanOrder>();
         PlanOrder planOrder = null;
